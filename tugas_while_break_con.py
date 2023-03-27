22528b85ba656087b18062bff8fe9a89990a0c7d
Firman Maulana <114965959+Firmanstmik@users.noreply.github.com>
2023-03-27 21:39:23 +0800

    tugas_while_break_con.py
    Tugas 2 While Python Struktur Data - Firman Maulana

---
+++ tugas_while_break_con.py
@@ -0,0 +1,16 @@
+print(27*"=")
+print("|| TUGAS 2 STRUKTUR DATA ||")
+print("|| NAMA : FIRMAN MAULANA ||")
+print(27*"=")
+
+angka = 0
+
+while angka < 15:
+    angka += 1
+    if angka > 7 and angka < 10:
+        continue
+    print(angka)
+    
+    
+
+    
